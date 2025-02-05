23b7047bb50b95e2d7173e7516d3dcd7174492cb
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-02-05 16:38:12 +0000

    Update Review1.java

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,31 +1,38 @@
 public class Review1 {
 
-	public static void main() {
+	public static void q1() {
 		//Write question 1 code here
 	
-		System.out.println("There once was a man from St. Ives");
+		System.out.println("There once was a man from St. Ives"); 
 	}
-}
-	/*public static void q2() {
+
+	public static void q2() {
 		//Write question 2 code here
-		int num1=5;
-		int num2=9;
-		System.out.println(num1/num2);
+		int num1=9;
+		int num2=5;
+		System.out.println(num1/num2); 
+		System.out.println(num1%num2);
 
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\n Hello Again"); 
 
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		System.out.println(true); 
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		 String a = "Hello ";
+		 String b = " World";
+		System.out.println(a + " ");
+		System.out.println(b);
+
 	}
 
 }
-*/
