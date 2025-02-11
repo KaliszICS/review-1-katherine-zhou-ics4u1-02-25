6325fe49cdb07c80c7fe0ea472f1fa41ec07cc78
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-02-11 03:10:56 +0000

    Update Review1

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,9 +1,20 @@
 public class Review1 {
 
+	public static void main(String[] args){
+		q1
+		q2
+		q3
+		q4
+		q5
+
+	
+
+	}
+
 	public static void q1() {
 		//Write question 1 code here
 	
-		System.out.println("There once was a man from St. Ives"); 
+		System.out.println("There once was a man from St. Ives."); 
 	}
 
 	public static void q2() {
@@ -17,19 +28,20 @@ public class Review1 {
 
 	public static void q3() {
 		//Write question 3 code here
-		System.out.println("Hello\n Hello Again"); 
+		System.out.println("Hello\nHello again"); 
 
 	}
 
 	public static void q4() {
 		//Write question 4 code here
 		System.out.println(true); 
+		
 	}
 
 	public static void q5() {
 		//Write question 5 code here
-		 String a = "Hello ";
-		 String b = " World";
+		 String a = "Hello";
+		 String b = "World";
 		System.out.print(a + " ");
 		System.out.println(b);
 
